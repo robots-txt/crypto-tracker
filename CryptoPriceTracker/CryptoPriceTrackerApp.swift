28392c2ff6d1740601ce8d92b65eadf36cc3c09a
Sanjay Ramanathan <sanjay316in@gmail.com>
2025-11-11 19:09:18 +1100

    initial commit

---
+++ CryptoPriceTracker/CryptoPriceTrackerApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct CryptoPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoPriceTracker/Shared/Constants/Fonts.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+extension Font {
+    static func appTitle() -> Font { .largeTitle.bold() }
+    static func headline() -> Font { .headline }
+    static func subheadline() -> Font { .subheadline }
+    static func body() -> Font { .body }
+    static func caption() -> Font { .caption }
+}
+
+extension CGFloat {
+    static let paddingSmall: CGFloat = 8
+    static let paddingMedium: CGFloat = 16
+    static let paddingLarge: CGFloat = 24
+    static let cornerRadius: CGFloat = 10
+    static let iconSize: CGFloat = 24
+}
